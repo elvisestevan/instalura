1e181a3d5113739d8cce6f49fa5d745dc80aae02
Elvis Estevan <elvis.estevan65@gmail.com>
2018-03-16 21:35:13 -0300

    Parte 1 da Aula 3, criando tela de login e apontando raíz para a tela de login usando react router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from "./components/Login";
 import "./css/login.css";
 import "./css/reset.css";
 import "./css/timeline.css";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(  
+(
+  <Router>
+    <Switch>
+      <Route path="/" component={Login} />
+      <Route path="/timeline" component={App} />
+    </Switch>
+  </Router>    
+), document.getElementById('root'));
